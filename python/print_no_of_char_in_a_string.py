75c4072af9101a7cc51f4969ab9d44e40f1238ed
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-08 19:43:01 +0530

    python: Print the number of characters in a string using Python
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I41840396b6d07e77884ebc548e6c352c3ac4d54b

---
+++ python/print_no_of_char_in_a_string.py
@@ -0,0 +1,9 @@
+#
+# Print the number of characters in a string using Python
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+print('Enter a String')
+str = input()
+length = len(str)
+print('The number of characters in the string:', length)
