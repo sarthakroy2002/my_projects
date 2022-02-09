e2503bec3ee1a9c70e423e4162f10b19628e23d6
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-09 11:27:50 +0530

    python: Convert celsius into Fahrenheit scale
    - Write a Python program to accept temperature in Celsius scale and convert it into Fahrenheit scale
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ib86233579ba40a9d3af4a06921cdd642821a7c66

---
+++ python/temperatureconv_1.py
@@ -0,0 +1,8 @@
+#
+# Write a Python program to accept temperature in Celsius scale and convert it into Fahrenheit scale
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+C = float(input("Temperature value in degree Celsius: " ))  
+F = (C * 1.8) + 32 
+print('The Fahrenheit temperature:',F)  
