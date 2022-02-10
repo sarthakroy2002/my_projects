1ba05467f6ae8cf6568b1dabe66402e8cf06af13
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-10 10:38:02 +0530

    python: Check odd or even using if else
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ib094e2821233673d810f80dbf60bd40e7c4bfc0c

---
+++ python/odd_even.py
@@ -0,0 +1,11 @@
+#
+# Write a Python program to check the number is Odd or Even
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+x= int(input("Enter an integer:"))
+r=x%2
+if r == 0:
+	print("Even Number")
+else:
+	print("Odd Number")
