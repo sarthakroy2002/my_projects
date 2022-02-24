b0267861dd8ed5eef367c686d6872c605f6922cb
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-24 10:40:32 +0530

    python: Add a series of number till n
    Change-Id: I06a59c6366ed4ceb2175727b01e1006857058424
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

---
+++ python/add_n_num.py
@@ -0,0 +1,12 @@
+#
+# Add a series of number till n
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+a=int(input("Enter a number:"))
+i=0
+o=0
+while i<a:
+    i=i+1
+    o=o+i 
+print("The Sum of the numbers are:", o)
