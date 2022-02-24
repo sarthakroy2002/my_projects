38b9ce9e8b83adf4deffb85455f8e935655c66b1
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-24 10:27:37 +0530

    python: Factorial of a number
    Change-Id: Ia727d94b55250827033f745bf6096af8b4e3fb1e
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

---
+++ python/factorial.py
@@ -0,0 +1,12 @@
+#
+# Factorial of a number using Python
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+a=int(input("Enter a number:"))
+f=1
+i=1
+while i<=a:
+    f=f*i
+    i=i+1
+print("The Factorial is:", f)
