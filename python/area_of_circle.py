bfc0ffad5fece0f46a051eb2db0bbf502649ecf7
Sarthak Roy <sarthakroy2002@gmail.com>
2022-03-02 12:17:18 +0530

    python: Accept radius of a circle and print the area

---
+++ python/area_of_circle.py
@@ -0,0 +1,8 @@
+#
+# Write a Python program to accept radius of a circle and print the area
+#
+# author: Sarthak Roy <sarthakroy2002@gmail.com>
+#
+r=float(input("Enter radius:"))
+a=3.14*r*r
+print("The area is :", a)
